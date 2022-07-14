113130faf46321e7f5924e4b78843fddfd598525
marat_bulekbayev <marat_bulekbayev@epam.com>
2022-07-14 15:09:21 +0600

    Initializing Spring 5 project

---
+++ src/main/java/guru/springframework/sfgsprintpetclinic/SfgSprintPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgsprintpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSprintPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSprintPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgsprintpetclinic/SfgSprintPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgsprintpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSprintPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
